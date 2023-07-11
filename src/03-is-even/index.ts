988d794f1cf3563c568c217e7b1fe95548dacf2e
Arturs Bondars <a.bondars27@gmail.com>
2023-07-11 20:40:52 +0000

    Updating wording of task 03 to avoid confusion

--- src/03-is-even/index.ts
+++ src/03-is-even/index.ts
@@ -3,7 +3,6 @@
  *
  * Determine if given number is even
  * without using a mathematic operator ( +, -, %, /, Math, ParseInt etc.)
- * nor a conditional operator.
  *
  * Examples:
  * isEven(4) === true
